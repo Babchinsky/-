69a1adadd9b3092fa2d3a6526c92e8a1ef999e22
Oleksii Babchynskyi <babchinsky.prog@gmail.com>
2023-02-15 14:30:31 +0200

    Cделал двумя способами

--- Homeworks/13 hw 15.02.2023 IEnumerable, IEnumerator, yield/13 hw 15.02.2023 IEnumerable, IEnumerator, yield/Library.cs
+++ Homeworks/13 hw 15.02.2023 IEnumerable, IEnumerator, yield/13 hw 15.02.2023 IEnumerable, IEnumerator, yield/Library.cs
@@ -7,10 +7,10 @@ using System.Threading.Tasks;
 
 namespace _13_hw_15._02._2023_IEnumerable__IEnumerator__yield
 {
-    internal class Library: IEnumerable, IEnumerator
+    internal class Library: IEnumerable //1 cпособо, IEnumerator
     {
         Book[] books;
-        int curpos = -1;
+        //int curpos = -1;   //1 способ
 
         public Library(int length) 
         {
@@ -38,45 +38,61 @@ namespace _13_hw_15._02._2023_IEnumerable__IEnumerator__yield
                 books[i].Show();
         }
 
+        
+        //////////////////////////////////////////////////////////////////////////////////////////////////////// Первый способ
+        //public IEnumerator GetEnumerator()
+        //{
+        //    //Console.WriteLine("\nВыполняется метод GetEnumerator");
+        //    // возвращается ссылка на объект класса, реализующего перечислитель
+        //    return this;
+        //}
 
+        ////Устанавливает перечислитель в его начальное положение, т. е. перед первым элементом коллекции
+        //public void Reset()
+        //{
+        //    //Console.WriteLine("\nВыполняется метод Reset");
+        //    curpos = -1;
+        //}
+        //public object Current // Получает текущий элемент в коллекции
+        //{
+        //    get
+        //    {
+        //        //Console.WriteLine("\nВыполняется свойство Current");
+        //        return books[curpos];
+        //    }
+        //}
+        //// Перемещает перечислитель к следующему элементу коллекции
+        //public bool MoveNext()
+        //{
+        //    //Console.WriteLine("\nВыполняется метод MoveNext");
+        //    if (curpos < books.Length - 1)
+        //    {
+        //        curpos++;
+        //        return true;
+        //    }
+        //    else
+        //    {
+        //        this.Reset();
+        //        return false;
+        //    }
+
+        //}
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+        //////////////////////////////////////////////////////////////////////////////////////////////////////// Первый способ
         public IEnumerator GetEnumerator()
         {
             //Console.WriteLine("\nВыполняется метод GetEnumerator");
-            // возвращается ссылка на объект класса, реализующего перечислитель
-            return this;
+            for (int i = 0; i < books.Length; i++)
+                yield return books[i];
+            // При обращении к оператору yield return будет сохраняться текущее местоположение.
+            // И когда foreach перейдет к следующей итерации для получения нового объекта, 
+            // итератор начнет выполнение с этого местоположения.
         }
 
-        #region enumerator
-        //Устанавливает перечислитель в его начальное положение, т. е. перед первым элементом коллекции
-        public void Reset()
-        {
-            //Console.WriteLine("\nВыполняется метод Reset");
-            curpos = -1;
-        }
-        public object Current // Получает текущий элемент в коллекции
-        {
-            get
-            {
-                //Console.WriteLine("\nВыполняется свойство Current");
-                return books[curpos];
-            }
-        }
-        // Перемещает перечислитель к следующему элементу коллекции
-        public bool MoveNext()
-        {
-            //Console.WriteLine("\nВыполняется метод MoveNext");
-            if (curpos < books.Length - 1)
-            {
-                curpos++;
-                return true;
-            }
-            else
-            {
-                this.Reset();
-                return false;
-            }
+        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-        }
-        #endregion enumerator
     }
 }
