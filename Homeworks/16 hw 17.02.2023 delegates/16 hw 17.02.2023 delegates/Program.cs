94dce1fb7c1a2f199397550d7b5b9d8e5de41e0b
Babchinsky <babchinsky.prog@gmail.com>
2023-02-17 19:50:58 +0200

    I'm working on it

---
+++ Homeworks/16 hw 17.02.2023 delegates/16 hw 17.02.2023 delegates/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16_hw_17._02._2023_delegates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
