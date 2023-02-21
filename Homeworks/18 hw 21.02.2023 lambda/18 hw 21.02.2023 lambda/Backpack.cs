42c485fcd1cd3c63d32b13f5ff4815cbe8386b80
Babchinsky <babchinsky.prog@gmail.com>
2023-02-21 16:03:51 +0200

    Done

---
+++ Homeworks/18 hw 21.02.2023 lambda/18 hw 21.02.2023 lambda/Backpack.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class Item
+{
+    public string Name { get; set; }
+    public double Volume { get; set; }
+}
+
+class Backpack
+{
+    public string Color { get; set; }
+    public string Manufacturer { get; set; }
+    public string Material { get; set; }
+    public double Weight { get; set; }
+    public double Capacity { get; set; }
+    public List<Item> Contents { get; set; } = new List<Item>();
+
+    public event Action<Item> Notify;
+
+    public void AddItem(Item item)
+    {
+        if (item.Volume > Capacity - GetUsedVolume())
+        {
+            throw new Exception("Not enough space in the backpack.");
+        }
+
+        Contents.Add(item);
+        Notify?.Invoke(item);
+    }
+
+    public double GetUsedVolume()
+    {
+        double usedVolume = 0;
+        foreach (Item item in Contents)
+        {
+            usedVolume += item.Volume;
+        }
+        return usedVolume;
+    }
+
+    public override string ToString()
+    {
+        return $"Backpack ({Color}, {Manufacturer}, {Material}, {Weight} kg, {GetUsedVolume()}/{Capacity} L)";
+    }
+}
--- Homeworks/18 hw 21.02.2023 lambda/18 hw 21.02.2023 lambda/Program.cs
+++ Homeworks/18 hw 21.02.2023 lambda/18 hw 21.02.2023 lambda/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
+using System.Security.Principal;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,9 +13,9 @@ namespace _18_hw_21._02._2023_lambda
         static void Main(string[] args)
         {
             //Task_1();
-            //Task_2(); 
+            //Task_2();
             //Task_3();
-            //Task_4();
+            Task_4();
             //Task_5();
             //Task_6();
         }
@@ -22,8 +23,92 @@ namespace _18_hw_21._02._2023_lambda
         delegate (int, int, int) Get(Color color);
         static void Task_1()
         {
-            Color color1 = Color.FromName("Red");      
+            Color color1 = Color.FromName("Blue");
+
+            Get RGB = delegate (Color color)
+            {
+                return (color.R, color.G, color.B);
+            };
+
+            Console.WriteLine(RGB(color1));
         }
-        
+        static void Task_2()
+        {
+            Backpack backpack = new Backpack
+            {
+                Color = "Blue",
+                Manufacturer = "Acme",
+                Material = "Nylon",
+                Weight = 1.2,
+                Capacity = 10.0
+            };
+            //void DisplayMessage(string message) => Console.WriteLine(message);
+            //backpack.Notify += DisplayMessage;
+            backpack.Notify += (item) => Console.WriteLine($"Added item {item.Name} ({item.Volume} litres) to the backpack.");
+
+            Item book = new Item { Name = "Book", Volume = 3.5 };
+            backpack.AddItem(book);
+
+            Item laptop = new Item { Name = "Laptop", Volume = 6.5 };
+            backpack.AddItem(laptop);
+
+            Item waterBottle = new Item { Name = "Water Bottle", Volume = 1.0 };
+
+            try
+            {
+                backpack.AddItem(waterBottle); // генерирует исключение
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Message: " + ex.Message);
+            }
+
+            Console.WriteLine(backpack);
+
+        }
+        static void Task_3()
+        {
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+
+            int count = numbers.Count(num => num % 7 == 0);
+            
+            Console.WriteLine(count);
+        }
+        static void Task_4()
+        {
+            int[] numbers = { -1, -2, 3, -4, 5, -6, 7};
+
+
+            int count = numbers.Count(num => num  > 0);
+            
+            Console.WriteLine(count);
+        }
+        static void Task_5()
+        {
+            int[] numbers = { -2, 5, 7, -2, -4, 7, -1 };
+
+            var uniqueNegativeNumbers = numbers.Where(n => n < 0).Distinct();
+
+            foreach (var number in uniqueNegativeNumbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+        static void Task_6()
+        {
+            string text = "The quick brown fox jumps over the lazy dog";
+            string wordToFind = "fox";
+
+            bool containsWord = text.Split(' ').Any(word => word.ToLower() == wordToFind.ToLower());
+
+            if (containsWord)
+            {
+                Console.WriteLine($"The text contains the word '{wordToFind}'.");
+            }
+            else
+            {
+                Console.WriteLine($"The text does not contain the word '{wordToFind}'.");
+            }
+        }
     }
 }
