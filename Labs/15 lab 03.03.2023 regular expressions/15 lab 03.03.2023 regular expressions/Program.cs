418e4d80249d35a8f9254db825153bff00b9617b
Babchinsky <babchinsky.prog@gmail.com>
2023-03-03 11:40:55 +0200

    Done

---
+++ Labs/15 lab 03.03.2023 regular expressions/15 lab 03.03.2023 regular expressions/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Net.WebRequestMethods;
+
+namespace _15_lab_03._03._2023_regular_expressions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task_1();
+            //Task_2();
+
+            //Task_7();
+            //Task_8();
+            //Task_9();
+            //Task_10();
+            //Task_11();
+            Task_12();
+        }
+        static void Task_1()
+        {
+            string input = "kristest93@gmail.com";
+            string pattern = @"(\w*)(@gmail.com)";
+            Regex regex = new Regex(pattern);
+
+            Match match = regex.Match(input);
+
+            Console.WriteLine(match.Value);
+        }
+        static void Task_2()
+        {
+            string input = "+38098-499-40-84";
+            string pattern = @"(\+*)(380)(\d{2})(-)(\d{3})(-)(\d{2})(-)(\d{2})";
+            Regex regex = new Regex(pattern);
+
+            Match match = regex.Match(input);
+
+            Console.WriteLine(match.Value);
+        }
+
+        static void Task_7()
+        {
+            string input = "192.0.255.11";
+            string pattern = @"(((([2][0-5][0-5])|([0|1]*[0-9][0-9])|([0-9]))(\.)){3})(([2][0-5][0-5])|([0|1]*[0-9][0-9])|([0-9]))";
+            Regex regex = new Regex(pattern);
+            Match match = regex.Match(input);
+            Console.WriteLine(match.Value);
+        }
+        static void Task_8()
+        {
+            string input = "Подсчёт количества гласных в заданой строке";
+            string pattern = @"[ё]|[у]|[е]|[ы]|[а]|[о]|[э]|[я]|[и]|[ю]";
+            Regex regex = new Regex(pattern);
+            Match match = regex.Match(input);
+
+            int count = 0;
+            // отображаем все совпадения
+            while (match.Success)
+            {
+                Console.WriteLine(match.Value);
+                count++;
+                // Переходим к следующему совпадению
+                match = match.NextMatch();
+            }
+
+            Console.WriteLine("Количество гласных: " + count);
+        }
+        static void Task_9()
+        {
+            string input = "https://www.regex101.com/r/qAvMmc/1";
+            string pattern = @"(http)[s *](://)www\.(\w+)\.(\w+)";
+            Regex regex = new Regex(pattern);
+            Match match = regex.Match(input);
+            Console.WriteLine(match.Value);
+        }
+        static void Task_10()
+        {
+            string input = "fsdfasfd123123";
+            string pattern = @"^\w+$";
+            Regex regex = new Regex(pattern);
+            Match match = regex.Match(input);
+
+            if (match.Success) Console.WriteLine("Значение является авлфавитно-цифровым");
+            else Console.WriteLine("Значение НЕ является авлфавитно-цифровым");
+        }
+        static void Task_11()
+        {
+            string input = "2:59:22";
+            string pattern = @"([1][0-9]|[2][0-3]|((0?)[0-9]):[0-5][0-9]|((0?)[0-9]))(:[0-5][0-9]|((0?)[0-9]))?";
+            Regex regex = new Regex(pattern);
+            Match match = regex.Match(input);
+            Console.WriteLine(match.Value);
+        }
+        static void Task_12()
+        {
+            string input = "99950";
+            string pattern = @"\d{5}";
+            Regex regex = new Regex(pattern);
+            Match match = regex.Match(input);
+            Console.WriteLine(match.Value);
+        }
+    }
+}
---
+++ Lessons/23. Регулярные выражения. Поиск файлов и папок/1. Regular Expressions/Regular Expressions_7/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Regular_Expressions_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "один два три четыре пять шесть";
+            Console.WriteLine("Исходная строка:\n{0}", input);
+            // В шаблоне используются 5 групп
+            string pattern = @"\b(\w+)(\s)(\w+)(\s)(\w+)\b";
+            string replacement = "$5$4$3$2$1"; 
+            // исходная строка заменяется на 5 групп
+            input = Regex.Replace(input, pattern, replacement);
+            Console.WriteLine("\nВидоизмененная строка: \n" + input);
+
+            input = "1000000 USD";
+            Console.WriteLine("\nИсходная строка:\n{0}", input);
+            pattern = @"\b(\d+)\s?USD";
+            replacement = "$$$1";    // $$ - подставляет литерал "$"
+            // исходная строка заменяется на 1 группу
+            input = Regex.Replace(input, pattern, replacement);
+            Console.WriteLine("\nВидоизмененная строка: \n" + input);   
+
+            input = "AABBCCDD";
+            Console.WriteLine("\nИсходная строка:\n{0}", input);
+            pattern = "C+";
+            replacement = "$`";
+            // найденный фрагмент текста заменяется на подстроку 
+            // входной строки до соответствия шаблона поиска
+            input = Regex.Replace(input, pattern, replacement);
+            Console.WriteLine("\nВидоизмененная строка: \n" + input);
+
+            input = "AABBCCDD";
+            Console.WriteLine("\nИсходная строка:\n{0}", input);
+            pattern = "C+";
+            replacement = "$'";
+            // найденный фрагмент текста заменяется на подстроку 
+            // входной строки после соответствия шаблона поиска
+            input = Regex.Replace(input, pattern, replacement);
+            Console.WriteLine("\nВидоизмененная строка: \n" + input);
+
+            input = "AABBCCDDEE";
+            Console.WriteLine("\nИсходная строка:\n{0}", input);
+            pattern = "(A+)(B+)(C+)";
+            replacement = "$+";
+            // найденный фрагмент текста заменяется на последнюю захваченную группу
+            input = Regex.Replace(input, pattern, replacement);
+            Console.WriteLine("\nВидоизмененная строка: \n" + input);
+
+            input = "AABBCC";
+            Console.WriteLine("\nИсходная строка:\n{0}", input);
+            pattern = "B";
+            replacement = "$_";
+            // входная строка заменяет все соответствия шаблона поиска
+            input = Regex.Replace(input, pattern, replacement);
+            Console.WriteLine("\nВидоизмененная строка: \n" + input);
+        }
+    }
+}
