336f42f55e59855fe7e06665a040df5babcfa72f
Babchinsky <babchinsky.prog@gmail.com>
2023-03-15 19:03:19 +0200

    Done

---
+++ Projects/Project 14.03.2023/Project 14.03.2023/Car.cs
@@ -0,0 +1,27 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_14._03._2023
+{
+    internal class Car
+    {
+        string name;
+        int year;
+
+        public Car()
+        {
+            LogManager.GetCurrentClassLogger().Info("Объект класса Car создан");
+        }
+
+        public Car(string name, int year)
+        {
+            this.name = name;
+            this.year = year;
+            LogManager.GetCurrentClassLogger().Info($"Объект класса Car ({name}, {year}) создан");
+        }
+    }
+}
---
+++ Projects/Project 14.03.2023/Project 14.03.2023/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLog;
+
+namespace Project_14._03._2023
+{
+    internal class Program
+    {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
+        static void Main(string[] args)
+        {
+            logger.Debug("Ошибка ввода данных");
+            logger.Info("Необходимо ввести данные в диапазоне от 0 до 100");
+
+            Car a = new Car();
+            Car b = new Car("Audi", 2002);
+        }
+    }
+}
