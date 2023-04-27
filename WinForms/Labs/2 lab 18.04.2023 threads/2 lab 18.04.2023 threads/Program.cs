4c24b46b5c204b995b6661b932fbb4220ca2399c
Alexsey Babchinskiy <alexseyb62@gmail.com>
2023-04-27 19:15:41 +0300

    Done

--- WinForms/Labs/2 lab 18.04.2023 threads/2 lab 18.04.2023 threads/Program.cs
+++ WinForms/Labs/2 lab 18.04.2023 threads/2 lab 18.04.2023 threads/Program.cs
@@ -1,15 +1,117 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Threading;
 
-namespace _2_lab_18._04._2023_threads
+class Program
 {
-    internal class Program
+    public static void Task1()
     {
-        static void Main(string[] args)
+        List<object> collection = new List<object>() { 1, "Hello", DateTime.Now };
+
+
+        Thread thread = new Thread(() =>
         {
+            foreach (object item in collection)
+            {
+                Console.WriteLine(item.ToString());
+            }
+        });
+        thread.Start();
+    }
+
+    class Bank
+    {
+        private int money;
+        private string name;
+        private int percent;
+        private readonly object lockObject = new object();
+
+        public Bank(int money, string name, int percent)
+        {
+            Money = money;
+            Name = name;
+            Percent = percent;
         }
+
+        public int Money
+        {
+            get { return money; }
+            set
+            {
+                lock (lockObject)
+                {
+                    money = value;
+                    StartThread();
+                }
+            }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                lock (lockObject)
+                {
+                    name = value;
+                    StartThread();
+                }
+            }
+        }
+
+        public int Percent
+        {
+            get { return percent; }
+            set
+            {
+                lock (lockObject)
+                {
+                    percent = value;
+                    StartThread();
+                }
+            }
+        }
+
+        private void StartThread()
+        {
+            Thread thread = new Thread(() =>
+            {
+                string data = $"{DateTime.Now}: Money={money}, Name={name}, Percent={percent}";
+
+
+                while (true)
+                {
+                    try
+                    {
+                        using (StreamWriter writer = new StreamWriter("log.txt", true))
+                        {
+                            // Ваш код записи в файл
+                            writer.WriteLine(data);
+                        }
+                        break;
+                    }
+                    catch (IOException ex)
+                    {
+                        // Файл уже используется другим процессом
+                        // Повторяем попытку через некоторое время
+                        Thread.Sleep(100);
+                    }
+                }
+            });
+            thread.Start();
+        }
+    }
+
+
+    public static void Task2()
+    {
+        Bank bank = new Bank(1800, "Monobank", 1);
+    }
+
+    static void Main()
+    {
+        //Task1();
+        Task2();
     }
 }
---
+++ WinForms/Projects/CryptoMonitor/CryptoMonitor/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+using RestSharp;
+using RestSharp.Authenticators;
+
+namespace CryptoMonitor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+
+            var binanceApi = new BinanceApi("7jp33mErccyk0kzHXO9LLU1WG96mPzj2i8gy3eJXXEJ7OglbQQ4BauS2HTaCrJPu", // API Key
+                "HnW0GjsxqWKSbu0eJw1f0CiKAXDdSXJYBQWKs6LSwFRLFwlm5NyyxaXT7WeGGmBM");                 // Secret Key
+
+            var price = binanceApi.GetPrice("BTCUSDT");
+
+            Console.WriteLine($"Price: {price}");
+        }
+    }
+
+
+
+
+    public class BinanceApi
+    {
+        private const string baseUrl = "https://api.binance.com/api/v3";
+
+        private readonly RestClient client;
+
+        public BinanceApi(string apiKey, string apiSecret)
+        {
+            client = new RestClient(baseUrl);
+            client.AddDefaultHeader("X-MBX-APIKEY", apiKey);
+        }
+
+        public decimal GetPrice(string symbol)
+        {
+            var request = new RestRequest($"/ticker/price?symbol={symbol}", RestSharp.Method.GET);
+
+            var response = client.Execute<BinancePrice>(request);
+
+            if (response.StatusCode != System.Net.HttpStatusCode.OK)
+            {
+                throw new Exception($"Failed to get price for {symbol}: {response.Content}");
+            }
+
+            return response.Data.Price;
+        }
+
+        private class BinancePrice
+        {
+            public decimal Price { get; set; }
+        }
+    }
+
+}
