70f79b47c0f318f35254fb9f2d816d924a69cb65
Alexsey Babchinskiy <alexseyb62@gmail.com>
2023-05-03 14:23:35 +0300

    Haven't Done

---
+++ Homeworks/9 hw 18.04.2023  System.Threading.Thread/9 hw 18.04.2023  System.Threading.Thread/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _9_hw_18._04._2023__System.Threading.Thread
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
--- WinForms/Homeworks/8 hw 17.04.2023 Explorer/Form1.cs
+++ WinForms/Homeworks/8 hw 17.04.2023 Explorer/Form1.cs
@@ -14,8 +14,6 @@ namespace Explorer
 {
     public partial class Form1 : Form
     {
-
-
         ImageList image_list1 = new ImageList(); // список изображений для хранения малых значков
         ImageList image_list2 = new ImageList(); // список изображений для хранения больших значков
         ImageList image_list3 = new ImageList(); // список изображений для хранения больших значков
@@ -24,6 +22,7 @@ namespace Explorer
         string[] files;// = Directory.GetFiles(pathToFolder);
         string[] directories;// = Directory.GetDirectories(pathToFolder);
         string pathToFolder;
+
         public Form1()
         {
             InitializeComponent();
