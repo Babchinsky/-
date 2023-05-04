745c5da7906e2a8d6abbe02dacbe42f92b81f2b1
Alexsey Babchinskiy <alexseyb62@gmail.com>
2023-05-04 17:27:03 +0300

    Done

---
+++ Homeworks/12 hw 21.04.2023 Exercises/12 hw 21.04.2023 Exercises/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _12_hw_21._04._2023_Exercises
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
