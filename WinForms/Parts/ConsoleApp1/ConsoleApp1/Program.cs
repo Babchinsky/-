99fd6891d61e05c4372cb5a13ab5a9f1b4b51dd9
Alexsey Babchinskiy <alexseyb62@gmail.com>
2023-05-09 10:59:50 +0300

    Done

---
+++ WinForms/Parts/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Point
+    {
+        int x;
+        int y;
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Point point = new Point();
+
+            unsafe
+            {
+                int a = 10;
+                int* ptr = &a;
+                Console.WriteLine(*ptr);
+            }
+        }
+    }
+}
