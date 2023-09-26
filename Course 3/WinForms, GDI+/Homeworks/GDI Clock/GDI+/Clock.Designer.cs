c849cf9f35fcad0ed99bd3077cec3d65fcc9ae3e
Alexsey Babchinskiy <alexseyb62@gmail.com>
2023-09-26 21:10:44 +0300

    Done

---
+++ Course 3/WinForms, GDI+/Homeworks/GDI Clock/GDI+/Clock.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace GDI_Clock
+{
+    partial class FormClock
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBoxClock = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxClock)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxClock
+            // 
+            this.pictureBoxClock.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBoxClock.Location = new System.Drawing.Point(0, 0);
+            this.pictureBoxClock.Name = "pictureBoxClock";
+            this.pictureBoxClock.Size = new System.Drawing.Size(284, 261);
+            this.pictureBoxClock.TabIndex = 0;
+            this.pictureBoxClock.TabStop = false;
+            // 
+            // FormClock
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.pictureBoxClock);
+            this.Name = "FormClock";
+            this.Text = "Clock";
+            this.Load += new System.EventHandler(this.FormClock_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxClock)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxClock;
+    }
+}
+
---
+++ Course 3/WinForms, GDI+/Homeworks/GDI Clock/GDI+/Clock.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+namespace GDI_Clock
+{
+    public partial class FormClock : Form
+    {
+        Graphics g;
+        Timer timer;
+        Point center;
+        Bitmap bmp;
+        Font font;
+        List<Point> masPointClock;
+        int ss;
+        int mm;
+        int hh;
+
+        public FormClock()
+        {
+            InitializeComponent();
+        }
+
+        private void FormClock_Load(object sender, EventArgs e)
+        {
+            masPointClock = new List<Point>();
+            center = new Point(ClientSize.Width / 2, ClientSize.Height / 2);
+            bmp = new Bitmap(ClientSize.Width, ClientSize.Height);
+            timer = new Timer();
+            timer.Interval = 1000;
+            timer.Tick += Timer_Tick;
+            timer.Start();
+            font = new Font("Arial", 8f);
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            ss = DateTime.Now.Second;
+            mm = DateTime.Now.Minute;
+            hh = DateTime.Now.Hour;
+            g = Graphics.FromImage(bmp);
+            g.Clear(Color.White);
+
+            // Smoothing(сглаживание)
+            g.SmoothingMode = SmoothingMode.AntiAlias;
+
+            // Border clock
+            for (int i = 0; i < 60; i++)
+            {
+                Point temp = new Point();
+                temp = ArrowCalc(center, i, 60);
+                masPointClock.Add(temp);
+              
+                if(i == 15 || i == 30 || i == 45 || i == 0)
+                    g.DrawLine(new Pen(Brushes.Black, 3), ArrowCalc(center, i, 75), ArrowCalc(center, i, 84));
+                if (i % 5 == 0 && i != 15 || i % 5 == 0 && i != 30 || i % 5 == 0 && i != 45 || i % 5 == 0 && i != 0)
+                    g.DrawLine(new Pen(Brushes.Black, 2), ArrowCalc(center, i, 75), ArrowCalc(center, i, 84));
+                else
+                    g.DrawLine(new Pen(Brushes.Black, 1), ArrowCalc(center, i, 79), ArrowCalc(center, i, 84));
+            }
+
+            
+
+            // Numbers
+            for (int i = 0; i < 60; i++)
+            {
+                if(i == 59)
+                    g.DrawString(" 12", font, Brushes.Black, ArrowCalc(center, i, 101));
+                if (i == 14)
+                    g.DrawString("3", font, Brushes.Black, ArrowCalc(center, i, 90));
+                if (i == 31)
+                    g.DrawString("  6", font, Brushes.Black, ArrowCalc(center, i, 90));
+                if (i == 46)
+                    g.DrawString("9", font, Brushes.Black, ArrowCalc(center, i, 104));
+            }
+           
+
+            // Center
+            g.DrawLine(new Pen(Brushes.Black, 4), center, new Point(ClientSize.Width/2 + 1, ClientSize.Height/2 + 1));
+
+
+            // Draw Cirle of Clock
+            g.DrawEllipse(new Pen(Brushes.Black, 2), (ClientSize.Width/2) - 87 , (ClientSize.Height/2) - 87 , 174, 174);
+
+
+            // Draw Arrows
+            Pen ssec = new Pen(Brushes.Black, 1);
+            ssec.SetLineCap(LineCap.NoAnchor, LineCap.ArrowAnchor, DashCap.Round);
+
+            Pen mmin = new Pen(Brushes.Black, 2);
+            mmin.SetLineCap(LineCap.NoAnchor, LineCap.ArrowAnchor, DashCap.Round);
+
+            Pen hhour = new Pen(Brushes.Black, 4);
+            hhour.SetLineCap(LineCap.NoAnchor, LineCap.ArrowAnchor, DashCap.Round);
+
+            g.DrawLine(ssec, center, ArrowCalc(center, ss, 80));
+            g.DrawLine(mmin, center, ArrowCalc(center, mm, 80));
+            g.DrawLine(hhour, center, ArrowCalcHour(center, hh, 60));
+
+            this.Text = $"{hh}:{mm}:{ss}";
+            pictureBoxClock.Image = bmp;
+            g.Dispose();
+        }
+
+        private Point ArrowCalc(Point cent, int val, int r)
+        {
+            int x = (int)(center.X + r * Math.Cos(-Math.PI / 2 + Math.PI * val / 30));
+            int y = (int)(center.Y + r * Math.Sin(-Math.PI / 2 + Math.PI * val / 30));
+            return new Point(x, y);
+        }
+
+        private Point ArrowCalcHour(Point cent, int val, int r)
+        {
+            int x = (int)(center.X + r * Math.Cos(-Math.PI / 2 + Math.PI * val / 6));
+            int y = (int)(center.Y + r * Math.Sin(-Math.PI / 2 + Math.PI * val / 6));
+            Point tt = new Point(x, y);
+            for (int i = 0; i < masPointClock.Count; i++)
+            {
+                if (tt == masPointClock[i])
+                {
+                    int tempCalc = mm / 12;
+                    return masPointClock[i + tempCalc];
+                }
+            }
+            return new Point(x, y);
+        }
+    }
+}
