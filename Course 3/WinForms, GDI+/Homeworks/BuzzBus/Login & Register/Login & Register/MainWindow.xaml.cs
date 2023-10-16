e40e5efecea55c552e1f105d66d422d694f1c69b
Babchinsky <babchinsky.prog@gmail.com>
2023-10-16 17:28:39 +0300

    Теперь шифрует и солит

--- Course 3/WinForms, GDI+/Homeworks/BuzzBus/Login & Register/Login & Register/MainWindow.xaml.cs
+++ Course 3/WinForms, GDI+/Homeworks/BuzzBus/Login & Register/Login & Register/MainWindow.xaml.cs
@@ -4,8 +4,8 @@ using System.Windows.Media;
 using System;
 using MongoDB.Driver;
 using MongoDB.Bson;
-
-
+using System.Security.Cryptography;
+using System.Text;
 
 namespace Login___Register
 {
@@ -25,8 +25,54 @@ namespace Login___Register
             IMongoDatabase database = client.GetDatabase("userbox");
             collection = database.GetCollection<BsonDocument>("users");
         }
-        
-        private void FindUsernameAndPassword(string username, string password) 
+
+        private string ComputeHash(string password, string salt)
+        {
+            //////////// Логика хеширования пароля с использованием соли
+            //////////// Здесь можно использовать хэширование, например, SHA-256
+            //////////// Пример простого хеширования (замените на соответствующий алгоритм):
+            //////////string hashedPassword = SomeHashFunction(password + salt);
+
+            //////////return hashedPassword;
+            ///
+
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                // Соль должна быть преобразована в байты
+                byte[] saltBytes = Encoding.UTF8.GetBytes(salt);
+
+                // Конкатенация пароля и соли
+                byte[] combinedBytes = new byte[saltBytes.Length + password.Length];
+                Array.Copy(saltBytes, 0, combinedBytes, 0, saltBytes.Length);
+                Array.Copy(Encoding.UTF8.GetBytes(password), 0, combinedBytes, saltBytes.Length, password.Length);
+
+                // Хеширование пароля с использованием SHA-256
+                byte[] hashBytes = sha256.ComputeHash(combinedBytes);
+
+                // Преобразование хеша в строку в формате Base64
+                string hashedPassword = Convert.ToBase64String(hashBytes);
+
+                return hashedPassword;
+            }
+        }
+
+        private string GenerateRandomSalt()
+        {
+            // Generate a random salt using a cryptographic random number generator
+            byte[] saltBytes = new byte[16]; // Adjust the size of the salt as needed
+            using (var rngCsp = new RNGCryptoServiceProvider())
+            {
+                rngCsp.GetBytes(saltBytes);
+            }
+
+            // Convert the salt to a base64-encoded string before returning
+            return Convert.ToBase64String(saltBytes);
+        }
+
+
+
+
+        private void FindUsernameAndPassword(string username, string password)
         {
             // Искомый username и пароль
             string usernameToSearch = username;
@@ -40,11 +86,15 @@ namespace Login___Register
 
             if (user != null)
             {
-                // Получаем пароль из базы данных
-                string passwordFromDatabase = user["Password"].AsString;
+                // Получаем хешированный пароль и соль из базы данных
+                string hashedPasswordFromDatabase = user["HashedPassword"].AsString;
+                string salt = user["Salt"].AsString;
 
-                // Проверяем соответствие паролей
-                if (passwordFromDatabase == passwordToCheck)
+                // Хешируем введенный пароль с использованием соли
+                string hashedPasswordToCheck = ComputeHash(passwordToCheck, salt);
+
+                // Проверяем соответствие хешированных паролей
+                if (hashedPasswordFromDatabase == hashedPasswordToCheck)
                 {
                     MessageBox.Show("Login successful.");
                 }
@@ -59,14 +109,20 @@ namespace Login___Register
             }
         }
 
+
         private void CheckAndAddUser(string username, string password)
         {
-            // Данные нового пользователя
-            string newUsername = username;
-            string newPassword = password; // Пароль в открытом виде
+            string name = "John Doe";
+            string email = "example@gmail.com";
+
+            // Генерация случайной соли для пароля
+            string salt = GenerateRandomSalt();
+
+            // Хеширование пароля с использованием соли
+            string hashedPassword = ComputeHash(password, salt);
 
             // Проверяем, что пользователь с таким именем не существует
-            var existingUserFilter = Builders<BsonDocument>.Filter.Eq("Username", newUsername);
+            var existingUserFilter = Builders<BsonDocument>.Filter.Eq("Username", username);
             var existingUser = collection.Find(existingUserFilter).FirstOrDefault();
             if (existingUser != null)
             {
@@ -76,18 +132,23 @@ namespace Login___Register
 
             // Создаем новый документ пользователя
             var newUserDocument = new BsonDocument
-        {
-            { "Username", newUsername },
-            { "Password", newPassword }
-            // Можно добавить другие данные пользователя, например, email, имя и т.д.
-        };
+            {
+                { "Username", username },
+                { "HashedPassword", hashedPassword }, // Захешированный пароль
+                { "Salt", salt }, // Соль
+                { "Name", name }, // Имя пользователя
+                { "Email", email }, // Email пользователя
+                { "RegistrationDate", DateTime.Now } // Дата регистрации
+                // Можно добавить другие данные пользователя, если необходимо
+            };
 
             // Добавляем нового пользователя в коллекцию
             collection.InsertOne(newUserDocument);
 
-            MessageBox.Show("Регистрация успешно завершена для пользователя с именем: " + newUsername);
+            MessageBox.Show("Регистрация успешно завершена для пользователя с именем: " + username);
         }
-    
+
+
 
         private void TextBox_GotFocus(object sender, RoutedEventArgs e)
         {
