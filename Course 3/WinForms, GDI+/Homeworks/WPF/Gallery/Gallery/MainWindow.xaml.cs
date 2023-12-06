38b1adf01bad644abab003fafb49c545ebeb37c0
Babchinsky <babchinsky.prog@gmail.com>
2023-12-06 19:36:47 +0200

    Сделал каркас Основного окна

---
+++ Course 3/WinForms, GDI+/Homeworks/WPF/Gallery/Gallery/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Gallery
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                // Обработка открытия файла
+                string selectedFilePath = openFileDialog.FileName;
+                // Добавьте свой код обработки файла здесь
+            }
+        }
+    }
+}
